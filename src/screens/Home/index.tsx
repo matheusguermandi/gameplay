d59a9a28687345255bb441797fb692aa1e8814ce
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-22 16:44:10 -0300

    add: CategorySelect component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 
 import { styles } from './styles';
+import { CategorySelect } from '../../components/CategorySelect';
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
       </View>
+
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
+
     </View>
   );
 }
