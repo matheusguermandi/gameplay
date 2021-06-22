f0b4afd812d177dd2e49d1af3aacd411a94d88d0
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-22 17:10:22 -0300

    update: Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
 
 import { styles } from './styles';
 import { CategorySelect } from '../../components/CategorySelect';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
 
 export function Home() {
   const [category, setCategory] = useState('');
@@ -13,6 +16,35 @@ export function Home() {
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
   }
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description:
+        'É hoje que vamos chegar ao challenger sem perder uma partida da md10',
+    },
+  ];
 
   return (
     <View>
@@ -26,6 +58,18 @@ export function Home() {
         setCategory={handleCategorySelect}
       />
 
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+
+        <FlatList
+          data={appointments}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 }
