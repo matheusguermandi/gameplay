b7eea40fa7232596e247db8116240d7c6fedfccf
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-23 17:00:24 -0300

    update: Category component

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -5,6 +5,7 @@ export const theme = {
     secondary100: '#0A1033',
     secondary90: '#0D133D',
     secondary80: '#0E1647',
+    secondary85: '#171F52',
     secondary70: '#1B2565',
     secondary60: '#1B2565',
     secondary50: '#243189',
@@ -23,5 +24,5 @@ export const theme = {
     title500: 'Rajdhani_500Medium',
     text400: 'Inter_400Regular',
     text500: 'Inter_500Medium',
-  }
+  },
 };
