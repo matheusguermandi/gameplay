7265f0a979d56807314792d42e38f5ecf4189fd9
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-24 23:39:46 -0300

    add: configs discord

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+const REDIRECT_URI ='https://auth.expo.io/@matheusguermandi/gameplay';
+const SCOPE = 'identify%20connections%20email%20guilds';
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '857798275393519647';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
