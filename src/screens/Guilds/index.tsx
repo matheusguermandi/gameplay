ad1db7ee0c5e74c72de2d2a7713a847fb15d6764
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-26 16:29:16 -0300

    Update: Guilds screen

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,70 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { View, FlatList } from 'react-native';
+
+import { Guild, GuildProps } from '../../components/Guild';
+import { Load } from '../../components/Load';
 import { ListDivider } from '../../components/ListDivider';
 
-import { GuildProps } from '../../components/Guild';
-import { Guild } from '../../components/Guild';
-
 import { styles } from './styles';
+import { api } from '../../services/api';
 
 type Props = {
   handleGuildSelect: (guild: GuildProps) => void;
 };
 
 export function Guilds({ handleGuildSelect }: Props) {
-  const guilds = [
-    {
-      id: '1',
-      name: 'Lendários',
-      icon: 'image.png',
-      owner: true,
-    },
-    {
-      id: '2',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true,
-    },
-    {
-      id: '3',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true,
-    },
-    {
-      id: '4',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true,
-    },
-    {
-      id: '5',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true,
-    },
-    {
-      id: '6',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true,
-    },
-  ];
+  const [guilds, setGuilds] = useState<GuildProps[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('/users/@me/guilds');
+      setGuilds(response.data);
+      setLoading(false);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={guilds}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <Guild data={item} onPress={() => handleGuildSelect(item)} />
-        )}
-        showsVerticalScrollIndicator={false}
-        ItemSeparatorComponent={() => <ListDivider />}
-        ListHeaderComponent={() => <ListDivider isCentered />}
-        contentContainerStyle={{ paddingBottom: 68, paddingTop: 50 }}
-        style={styles.guilds}
-      />
+      {loading ? (
+        <Load />
+      ) : (
+        <FlatList
+          data={guilds}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Guild data={item} onPress={() => handleGuildSelect(item)} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <ListDivider isCentered />}
+          ListHeaderComponent={() => <ListDivider isCentered />}
+          contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+          style={styles.guilds}
+        />
+      )}
     </View>
   );
 }
