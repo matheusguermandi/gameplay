456a248c6d5b42d09501c87cf136bc9fb5cbb8dd
matheusguermandi <matheus_guermandi@hotmail.com>
2021-06-25 23:50:24 -0300

    Update: Auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -33,7 +33,8 @@ type AuthProviderProps = {
 
 type AuthorizationResponse = AuthSession.AuthSessionResult & {
   params: {
-    access_token: string;
+    access_token?: string;
+    error?: string;
   };
 };
 
@@ -53,7 +54,7 @@ function AuthProvider({ children }: AuthProviderProps) {
         authUrl,
       })) as AuthorizationResponse;
 
-      if (type === 'success') {
+      if (type === 'success' && !params.error) {
         api.defaults.headers.authorization = `Bearer ${params.access_token}`;
 
         const userInfo = await api.get('/users/@me');
@@ -66,12 +67,11 @@ function AuthProvider({ children }: AuthProviderProps) {
           firstName,
           token: params.access_token,
         });
-        setLoading(false);
-      } else {
-        setLoading(false);
       }
     } catch {
       throw new Error('Não foi possível autenticar');
+    } finally {
+      setLoading(false);
     }
   }
 
